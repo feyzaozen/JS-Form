f7e3bec81f0734f0c9306bba055cf472ec7501f5
Feyza Ozen <feyzaozen2000@gmail.com>
2022-07-25 15:43:21 +0300

    function delete

--- main.js
+++ main.js
@@ -73,21 +73,7 @@ class KisiBilgileri {
     }
 }
 
-function addRowHandlers() {
-    var table = document.getElementById("KisiBilgileriTablo");
-    var rows = table.getElementsByTagName("tr");
-    for (i = 0; i < rows.length; i++) {
-      var currentRow = table.rows[i];
-      var createClickHandler = function(row) {
-        return function() {
-          var cell = row.getElementsByTagName("td")[0];
-          var id = cell.innerHTML;
-          alert("id:" + id);
-        };
-      };
-      currentRow.onclick = createClickHandler(currentRow);
-    }
-}
+
 
 
 function getValueInput(){
